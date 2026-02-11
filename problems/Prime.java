4dda905453277b4e0bbfb648d604d7080d7f2080
Amorto Goon <amorto.goon.121@gmail.com>
2026-02-11 14:06:05 +0600

    add simple prime number check

---
+++ problems/Prime.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Prime {
+
+    boolean isPrime(int n) {
+        if (n < 2)
+            return false;
+
+        for (int i = 2; i * i < n; i++) {
+            if (n % i == 2) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Prime p = new Prime();
+
+        int num = sc.nextInt();
+
+        if (p.isPrime(num)) {
+            System.out.printf("%d is a prime number\n", num);
+        } else {
+            System.out.printf("%d is not a prime number\n", num);
+        }
+    }
+}
